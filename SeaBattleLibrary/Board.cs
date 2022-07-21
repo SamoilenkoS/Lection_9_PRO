733a27a8035d5efa358b38cdfa230d222be5c988
Sviatoslav Samoilenko <Sviatoslav_Samoilenko@epam.com>
2022-07-21 22:05:22 +0300

    Added BL for SeaBattle

---
+++ SeaBattleLibrary/Board.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaBattleLibrary
+{
+    public class Board
+    {
+        private const int DefaultSize = 10;
+        public int Size { get; }
+        public Cell[,] Cells { get; private set; }
+
+        public Board(int size = DefaultSize)
+        {
+            Size = size;
+            InitializeCells();
+        }
+
+        private void InitializeCells()
+        {
+            Cells = new Cell[Size, Size];
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    Cells[i, j] = new Cell();
+                }
+            }
+        }
+
+        internal bool IsValidPlacement(//TODO add neighbours validation
+            ShipPlacementDetails shipPlacementDetails)
+        {
+            int xShift = shipPlacementDetails.IsHorizontal ? 1 : 0;
+            int yShift = shipPlacementDetails.IsHorizontal ? 0 : 1;
+            bool result = true;
+            for (int i = 0; i < shipPlacementDetails.Size; i++)
+            {
+                if (Cells[shipPlacementDetails.PlacementCoordinate.X + xShift * i,
+                    shipPlacementDetails.PlacementCoordinate.Y + yShift * i].Ship
+                    != null)
+                {
+                    result = false;
+                    break;
+                }
+            }
+
+            return result;
+
+        }
+
+        internal void PlaceShip(Ship ship)//TODO implement
+        {
+            throw new NotImplementedException();
+        }
+
+        internal ShotResult Shoot(Point shotCell)
+        {
+            if(
+                shotCell.X >= Size ||
+                shotCell.Y >= Size)
+            {
+                throw new ArgumentException();
+            }
+
+            var targetCell = Cells[shotCell.X, shotCell.Y];
+            if (targetCell.IsShooted)
+            {
+                return ShotResult.Incorrect;
+            }
+
+            targetCell.IsShooted = true;
+            return targetCell.Ship != null
+                ? ShotResult.Damaged
+                : ShotResult.Miss;
+        }
+    }
+}
---
+++ SeaBattleLibrary/GameEngine.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaBattleLibrary
+{
+    public class GameEngine
+    {
+        private Board _leftBoard;
+        private Board _rightBoard;
+        public bool LeftPlayerTurn { get; private set; }
+        public delegate void PropertyValueChanged();
+
+        public GameEngine()
+        {
+            _leftBoard = new Board();
+            _rightBoard = new Board();
+            LeftPlayerTurn = true;
+        }
+
+        public bool PlaceShip(bool leftBoard,
+            ShipPlacementDetails shipPlacementDetails)
+        {
+            Board target = GetBoard(leftBoard);
+
+            bool result = false;
+            if (target.IsValidPlacement(shipPlacementDetails))
+            {
+                Ship ship = GenerateShip(shipPlacementDetails);
+                target.PlaceShip(ship);
+                result = true;
+            }
+
+            return result;
+        }
+
+        private Ship GenerateShip(ShipPlacementDetails shipPlacementDetails)
+        {//TODO implement
+            throw new NotImplementedException();
+        }
+
+        private Board GetBoard(bool leftBoard)
+            => leftBoard ? _leftBoard : _rightBoard;
+
+        public ShotResult Shoot(Point shotCell)
+        {
+            Board target = GetBoard(LeftPlayerTurn);
+            ShotResult shotResult = target.Shoot(shotCell);
+
+            if(shotResult == ShotResult.Miss)
+            {
+                LeftPlayerTurn = !LeftPlayerTurn;
+            }
+
+            return shotResult;
+        }
+    }
+}
